b226612de325d34a9ceb7ed7822245f08389de47
wanma <wanmao1284879375@outlook.com>
2019-06-23 22:20:31 +0800

    vue-router的鈎子函數

--- src/components/Greet.vue
+++ src/components/Greet.vue
@@ -4,7 +4,19 @@
 
 <script>
 	export default {
-		name: "Greet"
+		name: "Greet",
+    beforeRouteEnter: (to, from, next) => {
+			console.log('Greet is going to enter')
+			console.log(to)
+			console.log(from)
+      next()
+    },
+    beforeRouteLeave: (to, from, next) => {
+	    console.log('Greet is going to leave')
+	    console.log(`Greet's to: `, to)
+	    console.log(`Greet's from: `, from)
+	    next()
+    }
 	}
 </script>
 
--- src/router.js
+++ src/router.js
@@ -20,7 +20,12 @@ export default new Router({
 			// route level code-splitting
 			// this generates a separate chunk (about.[hash].js) for this route
 			// which is lazy-loaded when the route is visited.
-			component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+			component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+			beforeEnter: (to, from, next) => {
+				console.log(`About's to: `, to);
+				console.log(`About's from: `, from);
+				next()
+			}
 		},
 		{
 			path: '/greet',
@@ -54,7 +59,7 @@ export default new Router({
 		},
 		{
 			path: '*',
-			component: ()=> import('./components/Err')
+			component: () => import('./components/Err')
 		}
 	]
 })
