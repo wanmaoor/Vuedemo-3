806b63b1145dc5a170b662b258ead7782d155a29
wanma <wanmao1284879375@outlook.com>
2019-06-24 10:53:42 +0800

    mapMutations

--- README.md
+++ README.md
@@ -16,6 +16,7 @@
 - 頁面的前進後退
 - Vuex基礎
 - 使用`mapState`把`state`放在組件的`script`裏
+- `mapMutations`
 
 ## Project setup
 ```
--- src/components/Vuex.vue
+++ src/components/Vuex.vue
@@ -2,23 +2,26 @@
  <div>
    <h1>{{num}}</h1>
    <p>
-     <button @click="$store.commit('increment')">Increment</button>
-     <button @click="$store.commit('decrement')">Decrement</button>
+     <button @click="increment(add)">Increment {{add}}</button>
+     <button @click="decrement(sub)">Decrement {{sub}}</button>
    </p>
  </div>
 </template>
 
 <script>
   import State from '@/store'
-  import {mapState} from 'vuex'
+  import {mapState, mapMutations} from 'vuex'
 	export default {
 		name: "Vuex",
     data(){
 			return {
-				State
+				State,
+        add: 10,
+        sub: 1
       }
     },
-    computed: mapState(['num'])
+    computed: mapState(['num']),
+    methods: mapMutations(['increment', 'decrement'])
 	}
 </script>
 
