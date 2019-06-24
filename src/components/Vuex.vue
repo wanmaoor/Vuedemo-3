410bacb6dde7d377176f5342dfe18ded37374b2f
wanma <wanmao1284879375@outlook.com>
2019-06-24 16:51:07 +0800

    modify statement issue about `...map*`

--- src/components/Vuex.vue
+++ src/components/Vuex.vue
@@ -29,10 +29,13 @@
 		computed: {
 			...mapState(['num']),
 			// ...mapGetters(['num'])
-			// 使用spread syntax是爲了不改變原對象.
-			// 因爲擴展運算符在對象裏的作用就是複製一個對象.
-			// 但是為這個新對象開闢了新的内存空間, 使得即便原對象改變也不會讓新對象發生改變.保持了原對象的完整性.
-			// 同時也符合計算屬性的設計理念.
+			// 使用spread syntax是爲了將多個對象合并為一個對象並返回給computed屬性, Be Hold:
+			// var a = {x: 1, y: 2}
+			// var b = {z: 3}
+			// var obj = {...a, ...b}
+			// obj => {x: 1, y: 2, z: 3}
+			// var obj1 = {a, b}
+			// obj1 => {{x: 1, y: 2},{z: 3}}
 		},
 		methods: {
 			...mapMutations(['increment', 'decrement']),
